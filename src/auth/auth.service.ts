8396d57c807c544da145f5fccf227085461e89df
krimi <ny.kalash@gmail.com>
2022-03-04 09:56:52 +0100

    class-validator implementation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon2 from 'argon2';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signup() {
-    return await this.prisma.user.findMany({ where: { id: 1 } });
+  async signup(dto: AuthDto) {
+    try {
+      const { email, password } = dto;
+      const hash = await argon2.hash(password);
+      const newUser = await this.prisma.user.create({
+        data: {
+          email,
+          password: hash,
+          firstName: 'test1',
+        },
+      });
+      delete newUser.password;
+      return newUser;
+    } catch (err) {
+      console.log(err.message);
+    }
   }
+
   signin() {
     return 'signin';
   }
