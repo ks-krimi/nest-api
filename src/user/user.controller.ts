fb1b1e5135a159257a483189d6421c4f1de5241a
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-09-14 17:21:28 +0300

    fix(e2e): module import

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 
-import { GetUser } from 'src/auth/decorator';
-import { JwtAuthGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtAuthGuard } from '../auth/guard';
 
 @UseGuards(JwtAuthGuard) // make it global
 @Controller('users')
