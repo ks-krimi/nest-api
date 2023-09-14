cfe99868a3eacda4ed06292f1921fddeeb958ea6
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-09-14 16:18:02 +0300

    refactor: use custome decorator and custome jwt guard
    move jwt guard on the top of the class

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 
+import { GetUser } from 'src/auth/decorator';
+import { JwtAuthGuard } from 'src/auth/guard';
+
+@UseGuards(JwtAuthGuard) // make it global
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  me(@Req() req: Request) {
-    return req.user;
+  me(@GetUser() user: User) {
+    return user;
   }
 }
